472857ab59d376d4f2fd8439edaebd1ddc261525
tapnair <patrick.rainsberry@autodesk.com>
2016-10-10 11:03:32 -0700

    Cleaned Up UI
    Also set it to only archive the active project.
    Massive speed increase

--- Project Archiver.py
+++ Project Archiver.py
@@ -1,8 +1,11 @@
-#Author-Autodesk Inc.
-#Description-This is sample addin.
+#Author- Patrick Rainsberry
+#Description-This is sample addin showing how to export a project.
+# Use at your own risk... this is neither official or supported.
 
 import adsk.core, adsk.fusion, traceback
 import os
+
+from os.path import expanduser
 
 MAX_PROJECTS = 20
         
@@ -47,11 +50,12 @@ def destroyObject(uiObj, tobeDeleteObj):
         else:
             uiObj.messageBox('tobeDeleteObj is not a valid object')
 
-def exportFolder(rootFolder, outputFolder):
+def exportFolder(rootFolder, outputFolder, file_types):
     for folder in rootFolder.dataFolders:
         exportFolder(folder, outputFolder)
     for file in rootFolder.dataFiles:
-        openDoc(file, outputFolder)
+        if file.fileExtension == "f3d":
+            openDoc(file, outputFolder, file_types)
 
 def dupCheck(name):
     if os.path.exists(name):
@@ -60,27 +64,51 @@ def dupCheck(name):
         name = base + ext
         dupCheck(name)
     return name
-        
 
-def exportActiveDoc(outputFolder):
+# Creates directory and returns file name for settings file
+def getFileName(projectName):
+
+    # Get Home directory
+    defaultPath = expanduser("~")
+    defaultPath += '/ProjectArchiver/'
+    defaultPath += projectName
+    defaultPath += '/'
+    # Create if doesn't exist
+    if not os.path.exists(defaultPath):
+        os.makedirs(defaultPath)
+    
+    return defaultPath    
+
+def exportActiveDoc(outputFolder, file_types):
 
     app = adsk.core.Application.get()
     design = app.activeProduct 
     exportMgr = design.exportManager
 
-    export_name = outputFolder + app.activeDocument.name + '.stp'
-    export_name = dupCheck(export_name)
-    exportOptions = exportMgr.createSTEPExportOptions(export_name)
-    exportMgr.execute(exportOptions)
+    exportFunctions = [exportMgr.createIGESExportOptions, 
+                       exportMgr.createSTEPExportOptions, 
+                       exportMgr.createSATExportOptions, 
+                       exportMgr.createSMTExportOptions, 
+                       exportMgr.createFusionArchiveExportOptions,
+                       exportMgr.createSTLExportOptions]
+    export_extensions = ['.igs', '.step', '.sat', '.smt', '.f3d', '.stl']      
+    
+    for i in range(file_types.count):
+        export_name = ' '
+        if file_types.item(i).isSelected:
+            export_name = outputFolder + app.activeDocument.name + export_extensions[i]
+            export_name = dupCheck(export_name)
+            exportOptions = exportFunctions[i](export_name)
+            exportMgr.execute(exportOptions)
 
-def openDoc(dataFile, outputFolder):
+def openDoc(dataFile, outputFolder, file_types):
     app = adsk.core.Application.get()
     
     try:
         document = app.documents.open(dataFile, True)
         if document is not None:
             document.activate()
-            exportActiveDoc(outputFolder)
+            exportActiveDoc(outputFolder, file_types)
             
             # Causes Fusion 360 to crash?
             # document.close(False)
@@ -139,16 +167,20 @@ def run(context):
                     inputs = args.command.commandInputs
                     
                     outputPath = inputs.itemById('outputPath').value   
-                    projectName = inputs.itemById('projectSelect').selectedItem.name  
+                    file_types = inputs.itemById('file_type_input').listItems
+#                    # This is to implement selective export                   
+#                    projectName = inputs.itemById('projectSelect').selectedItem.name  
+#                    
+#                    allProjects = app.data.dataProjects
+#
+#                    for project in allProjects:
+#                        if project is not None:
+#                            if project.name == projectName:
+#                                exportProject = project
+#                                break
                     
-                    allProjects = app.data.dataProjects
-
-                    for project in allProjects:
-                        if project is not None:
-                            if project.name == projectName:
-                                exportProject = project
-                                break
-                    exportFolder(exportProject.rootFolder, outputPath)
+                    
+                    exportFolder(app.data.activeProject.rootFolder, outputPath, file_types)
                 
                 except:
                     if ui:
@@ -177,21 +209,40 @@ def run(context):
                     cmd.executePreview.add(onExecutePreview)
                     handlers.append(onExecutePreview)
 
+                    app = adsk.core.Application.get()
+#                    allProjects = app.data.dataProjects
+                    activeProject = app.data.activeProject
+                    projectName = activeProject.name
                     # Create a few inputs in the UI
                     commandInputs_ = cmd.commandInputs
-                    projectSelect = commandInputs_.addDropDownCommandInput('projectSelect', 'Select Project', 1)
-                    commandInputs_.addStringValueInput('outputPath', 'Output Path: ')                    
+                    projectSelect = commandInputs_.addStringValueInput('projectSelect', "Project to Archive: ", projectName)
+                    projectSelect.isReadOnly = True
                     
-                    app = adsk.core.Application.get()
-                    allProjects = app.data.dataProjects
+                    defaultPath = getFileName(projectName)
+                    commandInputs_.addStringValueInput('outputPath', 'Output Path: ', defaultPath)                    
                     
-                    currentProject = 0
-                    for project in allProjects:
-                        if project is not None:
-                            projectSelect.listItems.add(project.name, False)
-                            currentProject += 1
-                            if currentProject >= MAX_PROJECTS:
-                                break
+                    multiSelectionCommandInput_ = commandInputs_.addDropDownCommandInput('file_type_input', 'Export Types', adsk.core.DropDownStyles.CheckBoxDropDownStyle)
+
+                    multiSelectionCommandInputListItems_ = multiSelectionCommandInput_.listItems
+                    multiSelectionCommandInputListItems_.add('IGES', False)
+                    multiSelectionCommandInputListItems_.add('STEP', True)
+                    multiSelectionCommandInputListItems_.add('SAT', False)
+                    multiSelectionCommandInputListItems_.add('SMT', False)
+                    multiSelectionCommandInputListItems_.add('F3D', False)
+                    multiSelectionCommandInputListItems_.add('STL', False)
+            
+            
+                    
+
+
+#                   THis creates a list of projects.  too slow.  Just using active.
+#                    currentProject = 0
+#                    for project in allProjects:
+#                        if project is not None:
+#                            projectSelect.listItems.add(project.name, False)
+#                            currentProject += 1
+#                            if currentProject >= MAX_PROJECTS:
+#                                break
 
 
 #                    ui.messageBox('Panel command created successfully')
